101e1e81f392029920c62d8cf65e33c787e6b840
newt239 <ysti2019.win2020.st@gmail.com>
2023-07-30 19:15:05 +0900

    remove: execute migrate at connect file

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,9 +1,4 @@
 import { sql } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
-import { migrate } from "drizzle-orm/vercel-postgres/migrator";
-
-import drizzleConfig from "../../drizzle.config";
 
 export const db = drizzle(sql);
-
-migrate(db, { migrationsFolder: drizzleConfig.out });
