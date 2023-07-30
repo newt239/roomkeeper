bb3d104342cec02e33f79912b452864a565c8ee4
newt239 <ysti2019.win2020.st@gmail.com>
2023-07-30 16:00:38 +0900

    feature: add basic authentication

---
+++ src/app/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/:path*"],
+};
+
+export function middleware(req: NextRequest) {
+  if (!process.env.BASIC_AUTH_PASSWORD) {
+    return NextResponse.next();
+  }
+
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1] ?? "";
+    const [user, pwd] = atob(authValue).split(":");
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      pwd === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next();
+    }
+  }
+  url.pathname = "/api/basic-auth";
+
+  return NextResponse.rewrite(url);
+}
