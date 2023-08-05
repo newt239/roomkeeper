b3d996b4ba8548d997d025454a0e7110bb55d6a9
newt239 <ysti2019.win2020.st@gmail.com>
2023-08-05 21:15:17 +0900

    improve: focus direct input on change page

--- src/components/feature/DirectInput.tsx
+++ src/components/feature/DirectInput.tsx
@@ -24,6 +24,7 @@ export default function DirectInput() {
         })}
       >
         <Input
+          autoFocus
           name="user_id"
           onChange={(e) => setInputId(e.target.value)}
           type="text"
