4037776db7ad290acda7199e44db3e2a8a81bf04
newt239 <ysti2019.win2020.st@gmail.com>
2023-10-22 12:05:12 +0900

    docs: remove vercel postgres description

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import Title from "@/components/common/Title";
 
 export default function AdminPage() {
@@ -14,27 +12,6 @@ export default function AdminPage() {
         </a>
         等のクライアントソフトなどで直接接続することで、より高度な操作が可能です。
       </p>
-      <Title level="h4">データベースURLの確認方法</Title>
-      <ul>
-        <li>
-          <a href="https://vercel.com/dashboard" target="_blank">
-            Vercel Dashbboard
-          </a>
-          にログイン
-        </li>
-        <li>画面上部タブ内「Storage」から「roomkeeper-db」を選択</li>
-        <li>
-          「Quickstart」のコード右上にある「Show
-          Secret」をクリックした上で、「psql」のダブルクオーテーション内をコピー
-        </li>
-      </ul>
-      <Image
-        alt="データベースURLの確認方法"
-        height={867}
-        quality={100}
-        src="/admin/db-url-guide.webp"
-        width={1875}
-      />
     </div>
   );
 }
